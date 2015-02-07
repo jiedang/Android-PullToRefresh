446d0b8e3a1f000c3bd56d74d92f347b842e275a
dangjie <dangjie@meituan.com>
2015-02-07 11:42:01 +0800

    动画调整

--- library/src/com/handmark/pulltorefresh/library/internal/NewYearLoadingLayout.java
+++ library/src/com/handmark/pulltorefresh/library/internal/NewYearLoadingLayout.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
@@ -20,41 +21,14 @@ import com.nineoldandroids.view.ViewHelper;
 /**
  * Created by dangjie1 on 15/2/2.
  */
-public class NewYearLoadingLayout extends LoadingLayout{
-    private int frontViewHeiht = 0;
-    private int defaultHeiht = 0;
-    private static int mHeaderImageHeight = 0;
-    private View frontView;
+public class NewYearLoadingLayout extends LoadingLayout {
+    private int mHeaderImageHeight = 0;
     private ValueAnimator valueAnimator;
+
     public NewYearLoadingLayout(Context context, PullToRefreshBase.Mode mode, PullToRefreshBase.Orientation scrollDirection, TypedArray attrs) {
         super(context, mode, scrollDirection, attrs);
-        frontView = findViewById(R.id.new_year_bag_front);
-
-        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                if(frontViewHeiht == 0){
-                    frontViewHeiht =  frontView.getHeight();
-                }
-                if(defaultHeiht == 0){
-                    defaultHeiht = getHeight();
-                }
-                //因为LIST VIEW 滚动和刷新停留采用两个不同的VIEW 只有滚动的提前添加到了界面 停留采用LIST HEAD VIEW 添加  所以导致在停留VIEW 获取不到高度
-                // 1 保存高度
-                // 2 在获取高度失败后  直接设置记录值的位置
-                if(mHeaderImageHeight == 0 && mHeaderImage.getHeight() != 0){
-                    mHeaderImageHeight = mHeaderImage.getHeight();
-                }
-                if(mHeaderImage.getHeight() != 0){
-                    ViewHelper.setTranslationY(mHeaderImage, mHeaderImageHeight/4*3);
-                }else{
-                    ViewHelper.setTranslationY(mHeaderImage, mHeaderImageHeight/5);
-                }
-            }
-        });
-
-
+        mHeaderImageHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 75, context.getResources().getDisplayMetrics());
+        ViewHelper.setTranslationY(mHeaderImage, mHeaderImageHeight / 3 * 2);
 
     }
 
@@ -71,19 +45,20 @@ public class NewYearLoadingLayout extends LoadingLayout{
 
     @Override
     protected void onPullImpl(float scaleOfLayout) {
-        if(scaleOfLayout > 0.9f){
-            if(valueAnimator == null){
-                valueAnimator =  ValueAnimator.ofFloat(mHeaderImageHeight/4*3,mHeaderImageHeight/5).setDuration(250);
+        if (scaleOfLayout > 0.9f) {
+            if (valueAnimator == null) {
+                valueAnimator = ValueAnimator.ofFloat(mHeaderImageHeight / 4 * 3, mHeaderImageHeight / 5).setDuration(250);
                 valueAnimator.start();
-            }else{
-                float currentValue = (Float)valueAnimator.getAnimatedValue();
-                if(ViewHelper.getTranslationY(mHeaderImage) != currentValue){
+            } else {
+                float currentValue = (Float) valueAnimator.getAnimatedValue();
+                if (ViewHelper.getTranslationY(mHeaderImage) != currentValue) {
                     ViewHelper.setTranslationY(mHeaderImage, currentValue);
                 }
             }
         }
 
     }
+
     @Override
     protected void pullToRefreshImpl() {
         setVisibility(View.VISIBLE);
@@ -91,11 +66,16 @@ public class NewYearLoadingLayout extends LoadingLayout{
 
     @Override
     protected void refreshingImpl() {
-        ((AnimationDrawable) mHeaderImage.getDrawable()).start();
+        showNewYearAnimator();
     }
 
     @Override
     protected void releaseToRefreshImpl() {
+        showNewYearAnimator();
+    }
+
+    private void showNewYearAnimator() {
+        ViewHelper.setTranslationY(mHeaderImage, mHeaderImageHeight / 5);
         ((AnimationDrawable) mHeaderImage.getDrawable()).start();
     }
 
@@ -103,8 +83,8 @@ public class NewYearLoadingLayout extends LoadingLayout{
     protected void resetImpl() {
         setVisibility(View.INVISIBLE);
         ((AnimationDrawable) mHeaderImage.getDrawable()).stop();
-        if(mHeaderImageHeight > 0){
-            ViewHelper.setTranslationY(mHeaderImage, mHeaderImageHeight/4*3);
+        if (mHeaderImageHeight > 0) {
+            ViewHelper.setTranslationY(mHeaderImage, mHeaderImageHeight / 3 * 2);
         }
         valueAnimator = null;
     }
